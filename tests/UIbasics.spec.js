dca1e0281b5ceaf4fdabf20d81a325b5019429ac
Thirupathireddy <thirupathireddy6622@gmail.com>
2026-08-13 18:44:05 +0530

    Get page title

--- tests/UIbasics.spec.js
+++ tests/UIbasics.spec.js
@@ -4,4 +4,7 @@ const {test} = require('playwright@test');
             const context = await browser.newContext();
             const page = await context.newPage();
             await page.goto("https://playwright.dev/");
+            /*Expect a title "to contain" a substring.*/
+            await expect(page).toHaveTitle(/Playwright/);
+
         })
